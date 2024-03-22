3d10cedb01567f815cd29c90fc8094b9577072b6
whzd <whzdxl@gmail.com>
2024-03-22 21:06:24 +0000

    Add Chapter 10 lifetimes

---
+++ the_book/chapter10/lifetimes/src/main.rs
@@ -0,0 +1,63 @@
+use std::fmt::Display;
+
+#[allow(dead_code)]
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+#[allow(dead_code)]
+impl<'a> ImportantExcerpt<'a> {
+    // first elision rule applies so no need
+    // for lifetime annotations
+    fn level(&self) -> i32 {
+        3
+    }
+
+    // first and third elision rules applies so no need
+    // for lifetime annotations
+    fn annouce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
+
+#[allow(unused_variables)]
+fn main() {
+    /*
+    &i32        // a reference
+    &'a i32     // a reference with an explicit lifetime
+    &'a mut i32 // a mutable reference with an explicit lifetime
+    */
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+}
+
+#[allow(dead_code)]
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
